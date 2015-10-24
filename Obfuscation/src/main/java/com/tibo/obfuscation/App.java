445b712793e8c0e02ec4b0f2dca9aa35f63316af
thibaud <thibaud@Thibaud-PC>
2015-10-24 16:37:50 +0200

    test spoon OK

---
+++ Obfuscation/src/main/java/com/tibo/obfuscation/App.java
@@ -0,0 +1,13 @@
+package com.tibo.obfuscation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Obfuscation/target/generated-sources/spoon/com/tibo/obfuscation/App.java
@@ -0,0 +1,10 @@
+package com.tibo.obfuscation;
+
+
+public class App {
+    public static void main(java.lang.String[] args) {
+        System.out.println("Enter in the method main from the class App");
+        java.lang.System.out.println("Hello World!");
+    }
+}
+
